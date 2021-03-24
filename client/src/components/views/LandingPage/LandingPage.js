0ac70671761474862df2a5f437022c2323e52492
boorooksus <boorooksus@gmail.com>
2021-03-24 16:26:02 +0900

    컴파일 기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react'
 import { Button, Header, Segment, Grid, Form, TextArea, Icon } from 'semantic-ui-react'
+import Axios from 'axios';
 
 function LandingPage() {
 
     const [Code, setCode] = useState("")
+    const [RunButton, setRunButton] = useState('Run')
+    const [Result, setResult] = useState('. . .')
 
     const editor = (event) => {
         if(event.keyCode===9){
@@ -25,6 +28,28 @@ function LandingPage() {
 
     const test = (event) => {
         console.log(event)
+
+        setRunButton(<Icon loading name='spinner' />)
+
+        const body={
+            code: Code
+        }
+
+        Axios.post('/api/compiler', body)
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data);
+                    //setResult(response.data)
+                    var result = response.data.result;
+
+                    setResult(result.split('\n').map((str, index) => <p key={index}>{str}</p>))
+
+                } else {
+                    alert('오류가 발생했습니다.')
+                }
+
+                setRunButton('Run')
+            })
     }
 
 
@@ -34,31 +59,26 @@ function LandingPage() {
             <Header as='h1' dividing>
                 <a href="/"> Python Complier Web App </a>
             </Header>
-            <Grid columns={2}>
+            <Grid doubling columns={2} >
             <Grid.Row stretched>
-            <Grid.Column>
-                <Segment>
+            <Grid.Column >
+                <Segment >
                 <Form action = "/" method="post">
-                    <TextArea id="myCode" placeholder="# Enter your code" name="description" rows="20" onKeyDown={editor} onChange={codeHandler} value={Code}></TextArea>
+                    <TextArea id="myCode" placeholder="# Enter your code" name="description" rows="20" onKeyDown={editor} onChange={codeHandler} value={Code} ></TextArea>
                 </Form>
+                <br />
+
+                <Button primary onClick={test}>{RunButton}</Button>
                 </Segment>
             </Grid.Column>
 
             <Grid.Column>
                 <Segment>
-                    hello
+                    {Result}
                 </Segment>
             </Grid.Column>
             </Grid.Row>
             </Grid>
-        <br />
-
-        <Button primary onClick={test}>Run</Button>
-
-        <Button primary animated='fade' onClick={test}>
-            <Button.Content visible>hi</Button.Content>
-            <Button.Content hidden><Icon loading name='spinner' /></Button.Content>
-        </Button>
             
         </div>
     )
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+var fs = require('fs')
+var sanitizeHtml = require('sanitize-html');
+var {PythonShell} = require('python-shell');
 
 // application/x-www-form-urlencoded 데이터를 분석해서 가져올 수 있게함
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,8 +15,44 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.get('/api/compiler', (req, res) => {
-    res.send('Hello World!')
+app.post('/api/compiler', (req, res) => {
+
+  console.log(req.body);
+
+  // console.log('res: ', res)
+  // console.log('res.req: ', res.req)
+
+  var sanitizedCode = `import time, sys, base64;start = time.time();sys.stdout=open('./server/output.out', 'w', encoding='utf8');
+` + 
+  sanitizeHtml(req.body.code) +
+  `
+print();print('Running time: ',end='');print(time.time() - start)`;
+
+  fs.writeFile(`./server/exec.py`, sanitizedCode, function(err){
+    // 파이썬 파일을 실행
+    var options = {
+      mode: 'text',
+      pythonPath: '', 
+      pythonOptions: ['-u'],
+      scriptPath: `${__dirname}`,
+      args: [ 'value1', 'value2', 'value3'] // 전달인자(sys.argv[i] 사용하면 받을 수 있음)
+    };
+
+    PythonShell.run('/exec.py', options, function (err, results){
+      if (err) {
+        console.log('err: ', err.message);
+        return res.json({ success: false, errorMessage: err.message })
+      
+      } else{
+        fs.readFile('./server/output.out', 'utf8', function(err, data){
+          console.log('data: ', data);
+          return res.json({ success: true, result: data})
+        })
+      }
+    })
+
+  })
+
 });
 
 app.listen(port, () => {
